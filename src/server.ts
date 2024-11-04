987ca341fe4e503bd1c9beb9541115bf3745a46a
Joseph Matheus <josephmatheus@hotmail.com>
2024-11-04 08:49:20 -0300

    adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,28 @@ app.get("/movies", async (_, res) => {
   res.json(movies);
 });
 
+app.use(express.json());
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Falha ao cadastrar filme" });
+  }
+
+  res.status(201).send();
+});
+
 app.listen(port, () => {
   console.log(`Servidor em execução na porta ${port}`);
 });

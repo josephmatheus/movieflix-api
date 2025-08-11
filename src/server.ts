340a30bedf7e871ddeb8c53803855ab0afde58d8
josephmatheus <josephmatheus@hotmail.com>
2025-08-11 16:54:45 -0300

    :heavy_plus_sign: build: Instalação e configurações iniciais prisma

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import express from "express";
 const port = 3000;
 const app = express();
 
-app.get('/movies', (req, res) => {
-    res.send('Listagem de filmes')
-})
+app.get("/movies", (req, res) => {
+  res.send("Listagem de filmes");
+});
 
 app.listen(port, () => {
-    console.log(`Servidor iniciado em http://localhost:${port}/`);
-})
+  console.log(`Servidor iniciado em http://localhost:${port}/`);
+});

0e9bdf45ebb254c5b1aecc5fd424543eb6acefd9
josephmatheus <josephmatheus@hotmail.com>
2025-08-28 18:19:42 -0300

    Adicionado filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -90,6 +90,28 @@ app.delete("/movies/:id", async (req, res) => {
   }
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+  try {
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    res.status(500).send({message: "Falha ao filtrar filmes por gênero"})
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor iniciado em http://localhost:${port}/`);
 });
